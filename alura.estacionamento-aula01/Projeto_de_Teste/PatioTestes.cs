164810971501392ff36ea43cf8a0add08de506f0
RafahsCunha <rafaelhsc20@gmail.com>
2023-03-06 15:15:58 -0300

    Anotação [Theory] e [InlineData] do xUnit

--- alura.estacionamento-aula01/Projeto_de_Teste/PatioTestes.cs
+++ alura.estacionamento-aula01/Projeto_de_Teste/PatioTestes.cs
@@ -31,9 +31,36 @@ namespace Projeto_de_Teste
             //Act
             double faturamento = estacionamento.TotalFaturado();
 
-            //Arrenge
+            //Assert
             Assert.Equal(2,faturamento);
 
+        }
+        [Theory] // Anotação que permite trabalhar com um grande conjunto de dados e testar vários métodos ao mesmo tempo 
+
+        [InlineData("André Gomes", "FGA-1325", "Preto", "Versa")]// InlineData passa os valores para os parâmetros do método que será testado
+        [InlineData("Joao Macedo", "JKK-4564", "Prata", "Voiage")]
+        [InlineData("Olivia Souza", "HUJ-5642", "Preto", "Onix")]
+
+        
+        public void ValidaFaturamentoVariosVeiculos(string proprietario, string placa, string cor, string modelo)
+        {
+            //Arrenge
+            var veiculo = new Veiculo();
+            veiculo.Proprietario = proprietario;
+            veiculo.Placa = placa;
+            veiculo.Cor = cor;
+            veiculo.Modelo = modelo;
+
+            var estacionamento = new Patio();
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+
+            //Act
+            var faturamento = estacionamento.TotalFaturado();
+
+            //Assert
+            Assert.Equal(2, faturamento);
         }
     }
 }
